26025f9e69a8b5763fab1462d558bc7d1fbe106f
dhtmddms <dhtmddms4043@naver.com>
2022-08-04 09:19:23 +0900

    임시커밋

--- VerDeTerr/src/main/java/com/board/controller/SurveyController.java
+++ VerDeTerr/src/main/java/com/board/controller/SurveyController.java
@@ -1,10 +1,8 @@
 package com.board.controller;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
