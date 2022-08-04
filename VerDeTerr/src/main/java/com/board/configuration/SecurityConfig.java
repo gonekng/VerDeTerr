ed85f39583a074514e4cce5746f63e85a03423d0
kimyoungjae <kimyoungjae123@naver.com>
2022-08-04 16:04:29 +0900

    게시판 기능 css js 적용. 기능테스트 완료.

--- VerDeTerr/src/main/java/com/board/configuration/SecurityConfig.java
+++ VerDeTerr/src/main/java/com/board/configuration/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.board.configuration;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,14 +24,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable() // post 방식으로 값을 전송할 때 token을 사용해야하는 보안 설정을 해제
 
-		.authorizeRequests().antMatchers("/login", "/logout", "/signup","/main","/signup_proc","/login_proc","/mypage","/identify","/identify_proc", "/css/**", "/img/**", "/js/**" , "/survey/surveylist.do","survey/suveyresult.do","/board/list.do","/board/write.do","/board/register.do","/board/view.do","/board/delete.do")
-				.permitAll()
-				.anyRequest().authenticated().and()
-				.formLogin().loginPage("/login").permitAll();
-	
-				
+				.authorizeRequests()
+				.antMatchers("/login", "/logout", "/signup", "/main", "/signup_proc", "/login_proc", "/mypage",
+						"/identify", "/identify_proc", "/**", "/css/**", "/img/**", "/js/**","/scripts/**","/plugin/**","/survey/surveylist.do",
+						"survey/suveyresult.do", "/board/list.do", "/board/write.do", "/board/register.do",
+						"/board/view.do", "/board/delete.do")
+				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
+
 	}
-	
+
 	/*
 	 * //static 파일 권한 허용 설정
 	 * 
