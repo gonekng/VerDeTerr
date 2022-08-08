7d3b5e47198866ac8bcb90b5393b4e74cea75d1c
jangminjee <jmj3047@naver.com>
2022-08-08 18:02:16 +0900

    0808

--- VerDeTerr/src/main/java/com/board/domain/MailDTO.java
+++ VerDeTerr/src/main/java/com/board/domain/MailDTO.java
@@ -8,7 +8,35 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MailDTO {
-    private String address;
+    public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getStr() {
+		return str;
+	}
+	public void setStr(String str) {
+		this.str = str;
+	}
+	@Override
+	public String toString() {
+		return "MailDTO [address=" + address + ", title=" + title + ", message=" + message + ", str=" + str + "]";
+	}
+	private String address;
     private String title;
     private String message;
     private String str;
