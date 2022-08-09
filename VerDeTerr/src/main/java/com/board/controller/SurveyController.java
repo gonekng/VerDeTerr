944042643dd5dac3324dbe5bed748dd25d913eae
dhtmddms <dhtmddms4043@naver.com>
2022-08-09 14:18:04 +0900

    메인 페이지 css 변경

--- VerDeTerr/src/main/java/com/board/controller/SurveyController.java
+++ VerDeTerr/src/main/java/com/board/controller/SurveyController.java
@@ -1,10 +1,5 @@
 package com.board.controller;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import javax.servlet.http.HttpSession;
 
 //import org.python.core.io.BufferedReader;
