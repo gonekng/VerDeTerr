469fa2aa1f56e826c1ebb30c03eacc1bcbff2058
dhtmddms <dhtmddms4043@naver.com>
2022-08-12 09:20:24 +0900

    충돌해결을 위한 커밋

--- VerDeTerr/src/main/python/test2.py
+++ VerDeTerr/src/main/python/test2.py
@@ -6,10 +6,9 @@ train = pd.read_csv('MBTI_train.csv', encoding='ISO 8859-1', header=None, names=
 train2 = pd.DataFrame()
 for i, type in enumerate(train['type'].unique()):
     df = train.loc[train['type']==type]
-    if(i%2==0):
-        train2 = pd.concat([train2, df.head(20)])
-    else:
-        train2 = pd.concat([train2, df.tail(20)])
+    df = df.sample(n=20, replace=True)
+    df.reset_index(drop=True, inplace=True)
+    train2 = pd.concat([train2, df])
 
 print("train2")
 print(train2['type'].value_counts())
