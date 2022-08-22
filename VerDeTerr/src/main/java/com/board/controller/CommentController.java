76e9edfd999a53f6af289299f02680e1152e6725
kimyoungjae <kimyoungjae123@naver.com>
2022-08-22 11:40:19 +0900

    댓글삭제구현

--- VerDeTerr/src/main/java/com/board/controller/CommentController.java
+++ VerDeTerr/src/main/java/com/board/controller/CommentController.java
@@ -3,20 +3,27 @@ package com.board.controller;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.board.adapter.GsonLocalDateTimeAdapter;
+import com.board.domain.BoardDTO;
 import com.board.domain.CommentDTO;
+import com.board.domain.UserDTO;
 import com.board.service.CommentService;
+import com.board.service.UserService;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
@@ -30,9 +37,9 @@ public class CommentController {
 
 	@RequestMapping(value = { "/comments", "/comments/{idx}" }, method = { RequestMethod.POST, RequestMethod.PATCH })
 	public JsonObject registerComment(@PathVariable(value = "idx", required = false) Long idx, @RequestBody final CommentDTO params) {
-
 		JsonObject jsonObj = new JsonObject();
-
+		
+		
 		try {
 			if (idx != null) {
 				params.setIdx(idx);
@@ -65,5 +72,25 @@ public class CommentController {
 
 		return jsonObj;
 	}
+	
+	@DeleteMapping(value = "/comments/{idx}")
+	public JsonObject deleteComment(@PathVariable("idx") final Long idx) {
+		System.out.println("deletecontroller 입성");
+		JsonObject jsonObj = new JsonObject();
+
+		try {
+			boolean isDeleted = commentService.deleteComment(idx);
+			jsonObj.addProperty("result", isDeleted);
+
+		} catch (DataAccessException e) {
+			jsonObj.addProperty("message", "데이터베이스 처리 과정에 문제가 발생하였습니다.");
+
+		} catch (Exception e) {
+			jsonObj.addProperty("message", "시스템에 문제가 발생하였습니다.");
+		}
+
+		return jsonObj;
+	}
 
 }
+
